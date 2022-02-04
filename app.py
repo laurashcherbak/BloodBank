b891aaf829f6c72cf5c62e3a07e2f459579ad0b7
Laura Shcherbak <45738929+laurashcherbak@users.noreply.github.com>
2022-02-04 10:24:07 +0200

    Update app.py

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ app.secret_key='some secret key'
 #Config MySQL
 app.config['MYSQL_HOST']='sql11.freemysqlhosting.net'
 app.config['MYSQL_USER']='sql11469232'
-app.config['MYSQL_PASSWORD']='mqtNlcAvZS'#'lssnV(909%WYQ2Qu'
+app.config['MYSQL_PASSWORD']='mqtNlcAvZS'
 app.config['MYSQL_DB']='sql11469232'
 app.config['MYSQL_CURSORCLASS']='DictCursor'
 #init MySQL
